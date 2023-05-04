d0daae08a15503029b82f85370b82f250c2d335d
Arthurastronom <arhtur.zagidullin@yandex.ru>
2023-05-04 15:21:58 +0300

    added a new info

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 int main(){
 
-	std::cout<<"Hello world"<<std::endle:
-		return ;
+	std::cout<<"Hello world"<<std::endl;
+	std::cout<<"second line:";
+	returnhttps://drive.google.com/file/d/1x-aVsx7ABYcVQQ-hJyUSIhlb7LZ6md8b/
 
 
-}
+https://drive.google.com/file/d/1x-aVsx7ABYcVQQ-hJyUSIhlb7LZ6md8b/view?usp=share_link
